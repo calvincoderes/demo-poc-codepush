4af9720e6c8886650929d8ace4a6aba936c0a754
Calvin Coderes <calvin.c.r.coderes@gmail.com>
2024-02-28 07:43:19 +0800

    assoc1

--- android/app/src/main/java/com/awesomeproject/MainActivity.kt
+++ android/app/src/main/java/com/awesomeproject/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.awesomeproject
+package com.awesomeprojectAssoc1
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/awesomeproject/MainApplication.kt
+++ android/app/src/main/java/com/awesomeproject/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.awesomeproject
+package com.awesomeprojectAssoc1
 
 import android.app.Application
 import com.facebook.react.PackageList
